2a24c1e306a38bb40e191db9e3407b47a163db10
Arthur Henrique <77027468+arthurhenrique22@users.noreply.github.com>
2022-07-22 23:41:23 -0300

    Update Exercicio3.js

--- Exercicio3.js
+++ Exercicio3.js
@@ -1,3 +1,4 @@
+//**OBS: Linguagem baseada em JavaScript**
 //Primeiro passo declarar as váriaveis que serão usadas para armazenar os dados.
 
 //váriavel que vai armazenar o nome.
@@ -16,10 +17,10 @@ var endereco;
 //atribuição do nome.
 var nome = "José Almeida da Silva";
 //atribuição do cpf.
-var cpf = "12345678900";
+var cpf = 12345678900;
 //atribuição do rg.
-var rg = "9517530";
+var rg = 9517530;
 //atribuição da altura.
-var altura = "1,78";
+var altura = 1,78;
 //atribuição do endereço.
 var endereco = "Rua A, 380 – Centro – Recife/PE";
