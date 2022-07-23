f91affec97241faae52e265817349e4c7e797526
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-07-22 23:24:26 -0300

    Create Exercicio3.js

---
+++ Exercicio3.js
@@ -0,0 +1,22 @@
+//Primeiro passo declarar as váriaveis que serão usadas para armazenar os dados// .
+//Segundo passo é atribuir um dado a váriavel.
+
+var nome;
+
+var cpf;
+
+var rg;
+
+var altura;
+
+var endereco;
+
+var nome = "José Almeida da Silva";
+
+var cpf = "12345678900";
+
+var rg = "9517530";
+
+var altura = "1,78";
+
+var endereco = "Rua A, 380 – Centro – Recife/PE";
