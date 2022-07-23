45625efe205e49360c464c1f0025a386ba1da97a
Arthur Henrique <77027468+arthurhenrique22@users.noreply.github.com>
2022-07-22 23:36:45 -0300

    Update Exercicio3.js

--- Exercicio3.js
+++ Exercicio3.js
@@ -1,22 +1,25 @@
-//Primeiro passo declarar as váriaveis que serão usadas para armazenar os dados// .
-//Segundo passo é atribuir um dado a váriavel.
+//Primeiro passo declarar as váriaveis que serão usadas para armazenar os dados.
 
+//váriavel que vai armazenar o nome.
 var nome;
-
+//váriavel que vai armazenar o cpf.
 var cpf;
-
+//váriavel que vai armazenar o rg.
 var rg;
-
+//váriavel que vai armazenar a altura.
 var altura;
-
+//váriavel que vai armazenar o endereço.
 var endereco;
 
+//Segundo passo é atribuir um dado a váriavel.
+
+//atribuição do nome.
 var nome = "José Almeida da Silva";
-
+//atribuição do cpf.
 var cpf = "12345678900";
-
+//atribuição do rg.
 var rg = "9517530";
-
+//atribuição da altura.
 var altura = "1,78";
-
+//atribuição do endereço.
 var endereco = "Rua A, 380 – Centro – Recife/PE";
