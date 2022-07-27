f256204f9cdf502b5723744dc5ca57ed406193f4
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-07-27 20:49:51 -0300

    A

---
+++ modulo_2/exercicio1.py
@@ -0,0 +1,17 @@
+nome = str(input('Qual é o seu nome? '))
+nota1 = float(input('Escreva sua primeira nota: '))
+nota2 = float(input('Escreva sua segunda nota: '))
+nota3 = float(input('Escreva sua terceira nota: '))
+falta = int(input('Quantas vezes você faltou? '))
+
+media: float = (nota1 + nota2 + nota3)  / 3
+
+print('Media: ', media)
+
+if media < 7.0:
+    print('Infelizmente', nome,  'você foi reprovado')
+elif media < 11:
+    print('Parabéns,', nome, 'você foi aprovado com sucesso !!!')
+elif falta > 3:
+    print('Mas você foi reprovado por falta, pois faltou', falta, 'vezes')
+
