9048666f25e44ce87d0354b3e4606cf9a37f1d25
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-07-29 00:11:09 -0300

    exercicio2

--- modulo_2/exercicio2.py
+++ modulo_2/exercicio2.py
@@ -7,18 +7,19 @@ pesobruto = 6001
 pessoas = 4
 
 if 1 < rodas <= 3:
-    print('Veículos com duas ou três rodas, tem a habilitação do tipo A')
+    print('Veículos com duas ou três rodas, tem a habilitação categoria ')
 
 elif rodas == 4 and pesobruto <= 3500 and pessoas <= 8:
-    print('Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg,tem a habilitação do tipo B')
+    print('Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg,tem a habilitação categoria B')
 
 elif rodas >= 4 and 3500 <= pesobruto <= 6000:
-    print('Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg, tem a habilitação do tipo C')
+    print('Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg, tem a habilitação categoria C')
 
 elif rodas >= 4 and pessoas > 8:
-    print('Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas, tem a habilitação do tipo D')
+    print('Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas, tem a habilitação categoria D')
 
 elif rodas >= 4 and pesobruto > 6000:
-    print('Veículos com quatro rodas ou mais e com mais de 6000 kg, tem a habilitação do tipo E')
+    print('Veículos com quatro rodas ou mais e com mais de 6000 kg, tem a habilitação categoria E')
 
-print('essa é a melhor categoria de habilitação para o veículo')
+categoria = input(str('Qual foi a categoria informada? '))
+    print('Essa é a categoria mais adadequada para o você', categoria, 'dirija com cuidado !')
