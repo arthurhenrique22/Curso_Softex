cdace591cedfade8bdd78b1a41268157218e4854
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-04 16:20:47 -0300

    exercicio_2

--- modulo_3/exercicio_2.py
+++ modulo_3/exercicio_2.py
@@ -1,14 +1,14 @@
 def insertionSort(lista):
 
     for step in range(1, len(lista)):
-        key = lista[step]
+        chave = lista[step]
         i = step - 1
 
-        while i >= 0 and key < lista[i]:
+        while i >= 0 and chave < lista[i]:
             lista[i + 1] = lista[i]
             i = i - 1
 
-        lista[i + 1] = key
+        lista[i + 1] = chave
 
 
 vetor = [61, 59, 57, 55, 53, 51, 49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 29, 27, 25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
