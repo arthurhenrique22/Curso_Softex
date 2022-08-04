2fb54095216a6eae13b00ab7a63e3d26255844b9
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-04 16:19:13 -0300

    exercicio_2

--- modulo_3/exercicio_2.py
+++ modulo_3/exercicio_2.py
@@ -2,13 +2,13 @@ def insertionSort(array):
 
     for step in range(1, len(array)):
         key = array[step]
-        j = step - 1
+        i = step - 1
 
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
-            j = j - 1
+        while i >= 0 and key < array[i]:
+            array[i + 1] = array[i]
+            i = i - 1
 
-        array[j + 1] = key
+        array[i + 1] = key
 
 
 vetor = [61, 59, 57, 55, 53, 51, 49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 29, 27, 25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
