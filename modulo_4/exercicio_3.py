59baecf91e3f9d52de221ebc8f1c416d6365f0a7
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-10 20:57:47 -0300

    exercicio_3

---
+++ modulo_4/exercicio_3.py
@@ -0,0 +1,21 @@
+class getset:
+    def __int__(self):
+        self._id = 0
+
+    @property
+    def id(self):
+        print("Sua idade: ")
+        return self._id
+
+    @id.setter
+    def id(self, i):
+        if  (i < 18):
+            raise ValueError('Você ainda não é maior de idade')
+        print('Você é maior de idade')
+        self._id = i
+
+pt = getset()
+
+pt.id = 19
+
+print(pt.id)
