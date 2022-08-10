651bf95c7d0cc06d574f84887f55b39925c60045
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-10 19:38:25 -0300

    exercicio_2

---
+++ modulo_4/exercicio_2.py
@@ -0,0 +1,11 @@
+class Flamengo:
+  def __init__(self, framengo):
+    self.framengo = framengo
+
+
+  def exibir1(self):
+    print(self.framengo)
+
+
+fla = Flamengo('O FLAMENGO VENCE MAIS UMA PARTIDA DANDO UM BAILE !!')
+fla.exibir1()
