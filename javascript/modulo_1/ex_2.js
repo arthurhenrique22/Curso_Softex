c58d7052c666014466dc9906244aea0f61dd4801
Arthur Henrique <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-22 17:21:18 -0300

    Update ex_2.js

--- javascript/modulo_1/ex_2.js
+++ javascript/modulo_1/ex_2.js
@@ -5,4 +5,4 @@ console.log('Eu sou o', nome)
 
 //  Eu escolhi usar o editor Vscode para rodar o javascript por ser simples de usar além de ter extensões 
 //que facilitam a vida do programador, juntamente instalei o nodejs para conseguir rodar os programas de maneira mais simples
-//sem precisar usar o browser para testar o código.
+//sem precisar usar o browser para executar o código.
