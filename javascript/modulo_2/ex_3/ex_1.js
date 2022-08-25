6b2a3f78182b190c48ba089f2ab4702425e8582a
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-25 11:37:01 -0300

    v1

---
+++ javascript/modulo_2/ex_3/ex_1.js
@@ -0,0 +1,9 @@
+nota1 = 10
+nota2 = 4
+nota3 = 4
+
+media = (nota1 + nota2 + nota3) / 3
+if (media >= 7)
+    console.log("Aluno foi aprovado!!")
+else
+    console.log("Aluno foi reprovado")
---
+++ javascript/modulo_2/ex_3/ex_2.js
@@ -0,0 +1,9 @@
+nota1 = 10
+nota2 = 4
+pontos = 21
+
+
+media = (nota1 + nota2)
+resultado = (pontos - media)
+
+console.log("Falta", resultado, "para passar você passar")
