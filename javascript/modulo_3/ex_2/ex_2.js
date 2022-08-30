044c88f591afd3fea9fcd6c10ea049cafeb4494d
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-08-30 18:14:29 -0300

    v1

---
+++ javascript/modulo_3/ex_2/ex_2.js
@@ -0,0 +1,11 @@
+let funcao = function() {}
+
+let contabasica = function(a, b) {
+    return a + b
+}
+console.log(contabasica(5, 3))
+
+let contabasica2 = (a, b) => {
+    return a + b
+}
+console.log(contabasica2(50, 50))
